3958abbe2198f1ca3199d4a5c0b9c08fba27cd95
alastorid@gmail.com <alastorid@gmail.com>
2017-10-15 01:15:48 +0800

    dumphex

--- fdump.sh
+++ fdump.sh
@@ -10,6 +10,10 @@ function dump(){
 function list(){
   objdump -d $1|sed -n '/[0-9a-f]\{8,16\} <[^>]\+>/{s/^[^<]*<\([^>]*\)>.*$/\1/;p}' |sed '/_init\|.plt.got\|_start\|@plt\|deregister_tm_clones\|register_tm_clones\|__do_global_dtors_aux\|frame_dummy\|__libc_csu_fini\|_fini/d'
 }
+function dumphex(){
+  dump $1 $2| sed '1 d;/  [0-9a-f]/ s/^  [0-9a-f]\+:[\t ]\([0-9a-f]\{2\}\)\(\( [0-9a-f]\{2\}\)*\).*$/\1\2/'|tr '\n' ' '
+  echo ""
+}
 if [ $# -lt 1 ] ;then usage; exit ;fi
 
 case $1 in
@@ -18,6 +22,9 @@ case $1 in
   ;;
   -dump)
     dump $2 $3
+  ;;
+  -dumphex)
+    dumphex $2 $3
   ;;
   *)
     usage
