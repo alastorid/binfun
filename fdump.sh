30d59e492368b697e7e9428b3b1fd46ccbb2bfe2
alastorid@gmail.com <alastorid@gmail.com>
2017-10-15 00:45:04 +0800

    more fun

--- fdump.sh
+++ fdump.sh
@@ -5,7 +5,7 @@ function usage(){
     echo "  -list list all function name inside a elf binary"
 }
 function dump(){
-  objdump -d $1|sed -n '/<'$2'>/{p;:a;n;/[0-9a-f]\{8,16\} <[^>]\+>/q;p;ba};'
+  objdump -Mintel -d $1|sed -n '/<'$2'>/{p;:a;n;/[0-9a-f]\{8,16\} <[^>]\+>/q;p;ba};'
 }
 function list(){
   objdump -d $1|sed -n '/[0-9a-f]\{8,16\} <[^>]\+>/{s/^[^<]*<\([^>]*\)>.*$/\1/;p}' |sed '/_init\|.plt.got\|_start\|@plt\|deregister_tm_clones\|register_tm_clones\|__do_global_dtors_aux\|frame_dummy\|__libc_csu_fini\|_fini/d'
